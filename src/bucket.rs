4fc07de6d76e15d8779bfb8253368bb50bc4ac9c
Monius <m1105930979@gmail.com>
2024-04-28 02:14:38 +0800

    use polar

--- src/bucket.rs
+++ src/bucket.rs
@@ -1,11 +1,22 @@
+use std::io::Write;
 use polars::prelude::*;
+use rayon::prelude::*;
 
-pub fn sample(pq_path: &str, num_rows: usize) -> Result<(), Box<dyn std::error::Error>> {
+pub fn sample(pq_path: &str, batch_size: usize) -> Result<(), PolarsError> {
 
-    let df = LazyFrame::scan_parquet(pq_path, ScanArgsParquet::default())?
-        .select([all(),])
+    let df: DataFrame = LazyFrame::scan_parquet(pq_path, Default::default())?
+        .select([col("video")])
         .collect()?;
-    println!("{}", df);
 
+    let video_series = df.column("video")?.binary()?;
+
+    video_series.iter().enumerate().into_iter()
+        .par_bridge().for_each(|(i, video)| {
+            if let Some(video_data) = video {
+                let name = format!("{:04}.mp4", i);
+                let mut output_file = std::fs::File::create(name).unwrap();
+                let _ = output_file.write_all(video_data);
+            }
+        });
     Ok(())
 }
